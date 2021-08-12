190df6341e4c413dd9c628d85819e8898c3d72f2
DmitriySerobyan <DmitriySerobyan@yandex.ru>
2021-08-12 19:20:16 +0500

    move classified image

--- src/main/java/ru/serobyan/Main.java
+++ src/main/java/ru/serobyan/Main.java
@@ -1,24 +1,44 @@
 package ru.serobyan;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-//            var classifier = ImageClassifier.of("C:\\image\\training", "C:\\image\\testing");
-            var classifier = ImageClassifier.of("C:\\mnist_png\\training", "C:\\mnist_png\\testing");
+            var classifier = ImageClassifier.of("C:\\image\\training", "C:\\image\\testing");
             classifier.buildModel();
 
-            var trainingDataSetIterator = classifier.getTrainingDataSetIterator();
-            classifier.trainModel(trainingDataSetIterator);
+            var trainingData = classifier.getTrainingDataSetIterator();
+            classifier.trainModel(trainingData);
 
-            var testingDataSetIterator = classifier.getTestingDataSetIterator();
-            classifier.evaluatingModel(testingDataSetIterator);
-            
-            var label = classifier.predict(new File("C:\\mnist_png\\testing\\1\\5.png"));
-            label.ifPresent(System.out::println);
+            var testingData = classifier.getTestingDataSetIterator();
+            classifier.evaluatingModel(testingData);
+
+            var imageFolder = new File("D:\\PicturesU");
+            var imagesForClassification = imageFolder.listFiles();
+            for (var image : imagesForClassification) {
+                var labelOpt = classifier.predict(image);
+                labelOpt.ifPresent(label -> {
+                    moveImage(image, label);
+                });
+            }
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void moveImage(File image, String label) {
+        try {
+            Files.copy(
+                Paths.get(image.getAbsolutePath()),
+                Paths.get("D:\\classification_test\\" + label + "\\" + image.getName()),
+                StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
